82f9d320a1b52329e6e70ebf56d420b3ba212ba6
boyshawn <boyshawn@hotmail.com>
2014-03-09 00:07:26 +0800

    Implemented without test: Retrival of tweet data from file, and the various model

--- src/model/Tweet.java
+++ src/model/Tweet.java
@@ -1,95 +1,70 @@
 package model;
 
 import java.text.SimpleDateFormat;
+import java.util.List;
 
 public class Tweet {
-	//Attribute
+	// Attribute
 	private String text;
 	private SimpleDateFormat date;
 
 	private String geolocation;
 	private User tweeter;
-	
+
 	private boolean isRetweet;
-	private int retweetCount;
-	private User retweetFrom;
-	
-	private boolean isReplyTweet;
-	private User replyTo;
-	
-	//Constructor
-	public Tweet(String text, String date, String geolocation,
-			boolean isRetweet, int retweetCount, boolean isReplyTweet) {
+	private List<User> mentionedUsers;
+
+	// Constructor
+	public Tweet(String text, String date, String geolocation, User tweeter) {
 		this.text = text;
 		this.date = new SimpleDateFormat(date);
 		this.geolocation = geolocation;
-		this.isRetweet = isReplyTweet;
-		this.retweetCount = retweetCount;
-		this.isReplyTweet = isReplyTweet;
-	}
-	
-	public Tweet(String text, User tweeter, String date, String geolocation,
-			boolean isRetweet, User retweetFrom, int retweetCount,
-			boolean isReplyTweet, User replyTo) {
-		this.text = text;
-		this.date = new SimpleDateFormat(date);
-		this.geolocation = geolocation;
-		this.isRetweet = isReplyTweet;
-		this.retweetCount = retweetCount;
-		this.isReplyTweet = isReplyTweet;
 		this.tweeter = tweeter;
-		this.retweetFrom = retweetFrom;
-		this.replyTo = replyTo;
 	}
-	
-	public Tweet(String text, User tweeter, String date, String geolocation,
-			boolean isRetweet, User retweetFrom, int retweetCount) {
+
+	public Tweet(String text, String date, String geolocation, User tweeter,
+			List<User> mentionedUsers) {
 		this.text = text;
 		this.date = new SimpleDateFormat(date);
 		this.geolocation = geolocation;
-		this.isRetweet = isReplyTweet;
-		this.retweetCount = retweetCount;
 		this.tweeter = tweeter;
-		this.retweetFrom = retweetFrom;
+		this.mentionedUsers = mentionedUsers;
+		this.isRetweet = setRetweet();
 	}
-	
-	public Tweet(String text, User tweeter, String date, String geolocation,
-			boolean isReplyTweet, User replyTo) {
-		this.text = text;
-		this.date = new SimpleDateFormat(date);
-		this.geolocation = geolocation;
-		this.isReplyTweet = isReplyTweet;
-		this.tweeter = tweeter;
-		this.replyTo = replyTo;
-	}
-	
-	//Method
+
+	// Method
 	public String text() {
 		return text;
 	}
+
 	public SimpleDateFormat date() {
 		return date;
 	}
+
 	public String geolocation() {
 		return geolocation;
 	}
+
 	public User tweeter() {
 		return tweeter;
 	}
+
 	public boolean isRetweet() {
 		return isRetweet;
 	}
-	public int retweetCount() {
-		return retweetCount;
+
+	public List<User> mentionUser() {
+		return mentionedUsers;
 	}
-	public User retweetFrom() {
-		return retweetFrom;
+
+	private boolean setRetweet() {
+		boolean isRetweet = false;
+
+		if (this.text != null) {
+			if (text.contains("RT @")) {
+				isRetweet = true;
+			}
+		}
+		return isRetweet;
 	}
-	public boolean isReplyTweet() {
-		return isReplyTweet;
-	}
-	public User replyTo() {
-		return replyTo;
-	}
-	
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,37 +1,45 @@
 package model;
 
-public class User implements Comparable<User>{
-	//User Particular
-	private String id;
+public class User implements Comparable<User> {
+	// User Particular
+	private int id;
 	private String screenName;
 	private String location;
 	private String accountName;
-	
-	public User(String id, String screenName, String location, String accountName){
+
+	public User(int id, String screenName, String location, String accountName) {
 		this.id = id;
 		this.screenName = screenName;
 		this.location = location;
 		this.accountName = accountName;
 	}
-	
+
 	public String accountName() {
 		return accountName;
 	}
 
-	public String id(){
+	public int id() {
 		return this.id;
 	}
-	
-	public String screenName(){
+
+	public String screenName() {
 		return this.screenName;
 	}
-	
-	public String location(){
+
+	public String location() {
 		return this.location;
 	}
 
 	@Override
 	public int compareTo(User o) {
-		return this.accountName.compareTo(o.accountName);
+		return (this.id - o.id);
+	}
+
+	public boolean hasLocation() {
+		if (this.location == null) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
