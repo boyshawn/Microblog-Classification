8988ca2684f7c03c969885b332dc055692bb381e
boyshawn <boyshawn@hotmail.com>
2014-03-13 11:27:48 +0800

    Update the testing & training data for assignment

---
+++ src/training/TestDriver.java
@@ -0,0 +1,13 @@
+package training;
+
+import model.Tweets;
+
+public class TestDriver {
+	
+	public static void main(String[] args){
+		final String fileName = "";
+		Tweets tweets = new Tweets(fileName);
+		
+	}
+
+}
