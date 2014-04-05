843c58966f0938dd2f9ad6f121ae386ccd783da5
Francis Pang <boyshawn@hotmail.com>
2014-04-05 20:16:51 +0800

    Refactor: 	- Remove all the Twitter4j library 	- Shift all the file operation related method from Driver to File Helper

--- src/assignment3/Driver.java
+++ src/assignment3/Driver.java
@@ -1,17 +1,8 @@
 package assignment3;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
-import org.joda.time.LocalDate;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 public class Driver {
 
@@ -22,7 +13,7 @@ public class Driver {
 		int numberOfDaysToCrawlAfterActualMatch = 3;
 
 		try {
-			theUltimate(queryFile, outputFile,
+			FileHelper.theUltimate(queryFile, outputFile,
 					numberOfDaysToCrawlBeforeActualMatch,
 					numberOfDaysToCrawlAfterActualMatch);
 		} catch (IOException e) {
@@ -30,77 +21,5 @@ public class Driver {
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-	}
-
-	public static void theUltimate(String queryFile, String outputFile,
-			int daysBeforeMatch, int daysAfterMatch)
-			throws IOException, JSONException {
-		
-			List<SoccerMatch> allMatches = extractMatchesFromFile(queryFile);
-
-			File file = new File(outputFile);
-			if(file.exists()){
-				file.delete();
-			}
-			file.createNewFile();
-
-			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-
-			for(SoccerMatch match : allMatches){
-				writer.write(assembleOneMatchJson(match, daysBeforeMatch, daysAfterMatch).toString());
-				writer.newLine();
-			}
-			
-			writer.close();
-	}
-
-	public static JSONObject assembleOneMatchJson(SoccerMatch soccerMatch,
-			int daysBeforeMatch, int daysAfterMatch)
-			throws JSONException {
-
-		LocalDate matchDate = soccerMatch.matchDate();
-		LocalDate querySince = matchDate.minusDays(daysBeforeMatch);
-		LocalDate queryUntil = matchDate.plusDays(daysAfterMatch);
-
-		List<String> homeTeamTweets = crawlOneTerm(soccerMatch.getHomeTeam().replaceAll(" ", "%20"),
-				querySince.toString(), queryUntil.toString());
-
-		List<String> awayTeamTweets = crawlOneTerm(soccerMatch.getAwayTeam().replaceAll(" ", "%20"),
-				querySince.toString(), queryUntil.toString());
-
-		JSONObject jsonMatch = new JSONObject();
-		JSONObject jsonSoccerMatch = new JSONObject(soccerMatch);
-
-		jsonMatch.put("Match Details", jsonSoccerMatch);
-		jsonMatch.put(soccerMatch.getHomeTeam(), homeTeamTweets);
-		jsonMatch.put(soccerMatch.getAwayTeam(), awayTeamTweets);
-
-		return jsonMatch;
-	}
-
-	public static List<String> crawlOneTerm(String query, String sinceDate,
-			String untilDate) {
-		
-		TweetCrawler tweetCrawler = new TweetCrawler(query, sinceDate,
-				untilDate);
-		return tweetCrawler.testIt();
-	}
-
-	public static List<SoccerMatch> extractMatchesFromFile(String filePath)
-			throws IOException {
-		
-		File file = new File(filePath);
-		List<SoccerMatch> soccerMatches = new ArrayList<SoccerMatch>();
-
-		BufferedReader reader = new BufferedReader(new FileReader(file));
-
-		String line;
-
-		while( (line=reader.readLine()) != null ){
-			if(!line.startsWith("//")){	//The file can contain comment
-				soccerMatches.add(SoccerMatch.extractOneSoccerMatch(line));
-			}
-		}
-		return soccerMatches;
 	}
 }
--- src/assignment3/FileHelper.java
+++ src/assignment3/FileHelper.java
@@ -3,145 +3,81 @@ package assignment3;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
-import java.util.Map;
 
 import org.joda.time.LocalDate;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import twitter4j.conf.Configuration;
-import twitter4j.conf.ConfigurationBuilder;
-
 public class FileHelper {
-	
-	/**
-	 * Extract all the query line by line from the query file
-	 * 
-	 * @param queryFile
-	 *            the file containing all the query terms
-	 * @return a list of query
-	 * @deprecated Not longer using this
-	 * 
-	 */
-	public static String[] retriveQueriesTermFromFile(File queryFile){
+	public static List<SoccerMatch> extractMatchesFromFile(String filePath)
+			throws IOException {
+
+		File file = new File(filePath);
+		List<SoccerMatch> soccerMatches = new ArrayList<SoccerMatch>();
+
+		BufferedReader reader = new BufferedReader(new FileReader(file));
 
 		String line;
-		BufferedReader reader = null;
 
-		try {
-			reader = new BufferedReader(new FileReader(queryFile));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		List<String> queries = new ArrayList<String>();
-
-		try {
-			while( (line = reader.readLine()) != null){
-				if(!line.startsWith("//")){	//The text file contain some comments
-					queries.add(line);
-				}
-
-			}
-			reader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (NullPointerException e){
-			e.printStackTrace();
-		}
-
-		return queries.toArray(new String[queries.size()]);	//Convert list to array
-	}
-
-	public static void writeToSingleFile(String baseDirectory, String fileName,
-			List<JSONObject> jsonQueryResult) throws IOException {
-
-		fileName = fileName.replace(' ', '_');
-
-		File outputFile = new File(baseDirectory + File.separator + fileName);
-
-		if(outputFile.exists()){
-			outputFile.delete();
-		}
-
-		outputFile.createNewFile();
-
-		if (outputFile.exists()) {
-			BufferedWriter writer = new BufferedWriter(
-					new FileWriter(outputFile));
-
-			for (JSONObject jsonStatus : jsonQueryResult) {
-				writer.write(jsonStatus.toString());
-				writer.newLine();
-			}
-
-			writer.close();
-		}
-	}
-
-	public static List<Configuration> readConfigurationFromFile(String filePath) throws Exception{
-		String line;
-		BufferedReader reader = null;
-		
-		reader = new BufferedReader(new FileReader(new File(filePath)));
-		List<Configuration> configurations = new ArrayList<Configuration>();
-		
-		String consumerKey = "";
-		String consumerSecret = "";
-		String accessToken = "";
-		String accessTokenSecret = "";
-		
-		while( (line = reader.readLine()) != null){
-			
-			String[] lineToken = line.split(" ");
-
-			if(line.contains("Consumer Key")){
-				consumerKey = lineToken[lineToken.length - 1];
-			}
-
-			else if(line.contains("Consumer Secret")){
-				consumerSecret = lineToken[lineToken.length - 1];
-			}
-			
-			else if(line.contains("Access Token Secret")){
-				accessTokenSecret = lineToken[lineToken.length - 1];
-			}
-			
-			else if(line.contains("Access Token")){
-				accessToken = lineToken[lineToken.length - 1];
-			}
-			
-			
-			if(	!consumerKey.isEmpty() && !consumerSecret.isEmpty() &&
-				!accessToken.isEmpty() && !accessTokenSecret.isEmpty()){
-
-				ConfigurationBuilder configBuilder = new ConfigurationBuilder();
-				
-				configBuilder.setOAuthConsumerKey(consumerKey);
-				configBuilder.setOAuthConsumerSecret(consumerSecret);
-				configBuilder.setOAuthAccessToken(accessToken);
-				configBuilder.setOAuthAccessTokenSecret(accessTokenSecret);
-				configBuilder.setJSONStoreEnabled(true);
-				
-				configurations.add(configBuilder.build());
-				
-				consumerKey = "";
-				consumerSecret = "";
-				accessToken = "";
-				accessTokenSecret = "";
+		while( (line=reader.readLine()) != null ){
+			if(!line.startsWith("//")){	//The file can contain comment
+				soccerMatches.add(SoccerMatch.extractOneSoccerMatch(line));
 			}
 		}
-		
 		reader.close();
+		return soccerMatches;
+	}
 
-		return configurations;
+	public static void theUltimate(String queryFile, String outputFile,
+			int daysBeforeMatch, int daysAfterMatch)
+					throws IOException, JSONException {
+
+		List<SoccerMatch> allMatches = FileHelper.extractMatchesFromFile(queryFile);
+
+		File file = new File(outputFile);
+		if(file.exists()){
+			file.delete();
+		}
+		file.createNewFile();
+
+		BufferedWriter writer = new BufferedWriter(new FileWriter(file), Integer.MAX_VALUE/30);
+
+		for(SoccerMatch match : allMatches){
+			writer.write(assembleOneMatchJson(match, daysBeforeMatch, daysAfterMatch).toString());
+			writer.newLine();
+		}
+
+		writer.close();
+	}
+	
+	private static JSONObject assembleOneMatchJson(SoccerMatch soccerMatch,
+			int daysBeforeMatch, int daysAfterMatch)
+			throws JSONException {
+
+		LocalDate matchDate = soccerMatch.matchDate();
+		LocalDate querySince = matchDate.minusDays(daysBeforeMatch);
+		LocalDate queryUntil = matchDate.plusDays(daysAfterMatch);
+
+		List<String> homeTeamTweets = TweetCrawler.crawlOneTerm(soccerMatch
+				.getHomeTeam().replaceAll(" ", "%20"), querySince.toString(),
+				queryUntil.toString());
+
+		List<String> awayTeamTweets = TweetCrawler.crawlOneTerm(soccerMatch
+				.getAwayTeam().replaceAll(" ", "%20"), querySince.toString(),
+				queryUntil.toString());
+
+		JSONObject jsonMatch = new JSONObject();
+		JSONObject jsonSoccerMatch = new JSONObject(soccerMatch);
+
+		jsonMatch.put("Match Details", jsonSoccerMatch);
+		jsonMatch.put(soccerMatch.getHomeTeam(), homeTeamTweets);
+		jsonMatch.put(soccerMatch.getAwayTeam(), awayTeamTweets);
+
+		return jsonMatch;
 	}
 }
