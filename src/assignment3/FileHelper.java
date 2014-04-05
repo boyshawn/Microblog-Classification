4fd9f45a4ab68210950f7e582db6ab9965852236
Francis Pang <boyshawn@hotmail.com>
2014-04-05 21:44:26 +0800

    Bug Fixed: Buffered Writer
    Issue: The size of the buffered writer cannot accomodate the buffer
    required for a match JSON object
    
    Work around: Manually break down the JSON and write smaller parts into
    the output file one by one.

--- src/assignment3/FileHelper.java
+++ src/assignment3/FileHelper.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.joda.time.LocalDate;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -48,7 +49,63 @@ public class FileHelper {
 		BufferedWriter writer = new BufferedWriter(new FileWriter(file), Integer.MAX_VALUE/30);
 
 		for(SoccerMatch match : allMatches){
-			writer.write(assembleOneMatchJson(match, daysBeforeMatch, daysAfterMatch).toString());
+			//writer.write(assembleOneMatchJson(match, daysBeforeMatch, daysAfterMatch).toString());
+			
+			//Break up the JSON and write
+			JSONObject jsonOneMatch = assembleOneMatchJson(match, daysBeforeMatch, daysAfterMatch);
+			
+			//Try to simulate a JSON format
+			writer.write('{');	//Starting
+			
+			//Home Team
+			writer.write("\"" + match.getHomeTeam() + "\":" );
+			writer.write('[');
+			
+			JSONArray homeTeamJsonArray = jsonOneMatch.getJSONArray(match.getHomeTeam());
+			
+			for(int i = 0; i < homeTeamJsonArray.length(); i++){
+				String tweet = (String) homeTeamJsonArray.get(i);
+				
+				writer.write('\"');
+				writer.write(tweet.replace("\"", " \\" + "\""));
+				writer.write('\"');
+				
+				if (i < (homeTeamJsonArray.length() - 1)) {
+					writer.write(',');
+				}
+			}
+			
+			writer.write(']');
+			writer.write(',');
+			
+			//Away Team
+			writer.write("\"" + match.getHomeTeam() + "\":" );
+			writer.write('[');
+			
+			JSONArray awayTeamJsonArray = jsonOneMatch.getJSONArray(match.getHomeTeam());
+			
+			for(int i = 0; i < awayTeamJsonArray.length(); i++){
+				String tweet = (String) awayTeamJsonArray.get(i);
+				
+				writer.write('\"');
+				writer.write(tweet.replace("\"", " \\" + "\""));
+				writer.write('\"');
+				
+				if (i < (awayTeamJsonArray.length() - 1)) {
+					writer.write(',');
+				}
+			}
+			
+			writer.write(']');
+			writer.write(',');
+			
+			//Match data
+			writer.write("\"Match Details\":");
+			JSONObject jsonMatch = jsonOneMatch.getJSONObject("Match Details");
+			writer.write(jsonMatch.toString());
+			
+			writer.write('}');	//End of JSON
+			
 			writer.newLine();
 		}
 
